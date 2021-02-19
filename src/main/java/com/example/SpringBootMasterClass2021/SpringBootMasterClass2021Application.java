bb727e7f50189554abff811a3574d36439ed667f
Michael Steinert <michael.steinert.94@gmail.com>
2021-02-19 20:40:15 +0100

    Initial Commit

---
+++ src/main/java/com/example/SpringBootMasterClass2021/SpringBootMasterClass2021Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMasterClass2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMasterClass2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMasterClass2021Application.class, args);
+	}
+
+}
