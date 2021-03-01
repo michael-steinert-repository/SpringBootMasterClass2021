2710f2cd4f702a7fe52aa24021fe6f25d2afcd3b
Michael Steinert <michael.steinert.94@gmail.com>
2021-03-01 21:50:51 +0100

    Added Logging

--- src/main/java/com/example/SpringBootMasterClass2021/customer/CustomerService.java
+++ src/main/java/com/example/SpringBootMasterClass2021/customer/CustomerService.java
@@ -1,11 +1,13 @@
 package com.example.SpringBootMasterClass2021.customer;
 
 import com.example.SpringBootMasterClass2021.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class CustomerService {
 
@@ -17,11 +19,16 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        log.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id) {
         return customerRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException(String.format("Customer with ID %s not found", id)));
+                .orElseThrow(() -> {
+                    NotFoundException notFoundException = new NotFoundException(String.format("Customer with ID %s not found", id));
+                    log.error("Error in getCustomers caused by Custmer {}", id, notFoundException);
+                    return notFoundException;
+                });
     }
 }
